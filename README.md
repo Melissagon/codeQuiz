e5e2da81111a14f100856a3669009ec3b421d978
Melissagon <110285166+Melissagon@users.noreply.github.com>
2022-10-12 22:09:51 -0400

    some js added. Start button activated.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # codeQuiz
+
+The purpose of this quiz it to help students identify areas in which they can improve. 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,74 @@
-//GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
 
-addEventListener("click")
+var startButton = document.querySelector(".start-button");
+var timer = document.querySelector(".timer");
+var scoreboard = document.querySelector(".scoreboard");
+var intro = document.querySelector(".intro");
+var questionsEl = document.querySelector(".questions")
+var score = 0;
+var timerEl;
+
+var questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
+startButton.addEventListener("click", startGame);
+
+function startGame (){
+    intro.style.display="none";
+    questionsEl.style.display="block";
+    var test = document.querySelector("#one")
+    test.textContent= questions[0].options[0]
+}
